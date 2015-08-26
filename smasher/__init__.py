d7e01e413eacd50256fd961a9f8d1aef1f827959
Jack Peterson <jack@tinybike.net>
2015-08-25 17:37:11 -0700

    Reorganization

---
+++ smasher/__init__.py
@@ -0,0 +1,7 @@
+__title__      = "smasher"
+__version__    = "0.1.0"
+__author__     = "Fox Peterson"
+__maintainer__ = "Fox Peterson"
+__email__      = "fox@tinybike.net"
+
+from .smasher import smashWorkers, form_connection
